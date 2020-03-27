ce0e93edbd313c0ca91ac334ec969ebd223ca88f
RicardoContreira <ricardocontreira23@hotmail.com>
2020-03-26 21:09:35 -0300

    origin
    back and da aplicação

---
+++ omni/backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const OngsController = require('./controllers/OngController');
+const IncidentController = require('./controllers/incidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngsController.index);
+routes.post('/ongs', OngsController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
